942f44e944bc48ca923e3919c4777d924bdfc7f1
Prem <tinkushakya2003@gmail.com>
2026-07-07 14:49:00 +0530

    Final Commit

--- back_end/routes/AuthRoutes.js
+++ back_end/routes/AuthRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 import { registerController,loginController, getAllUser, deleteUser } from "../controller/AuthController.js";
 import { requireSignIn } from "../middleware/authMiddleware.js";
 
-router.post("/register", registerController);
+router.post("/register",requireSignIn, registerController);
 router.post("/login", loginController);
 router.get("/all-user",requireSignIn, getAllUser);
 router.delete("/delete-user/:userId",requireSignIn, deleteUser);
